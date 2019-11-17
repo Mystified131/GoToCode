23f353aff0043117ed05068001cb16ad131a1b03
mystified131 <mystifiedthomas@gmail.com>
2019-11-17 15:17:37 -0600

    Tweaking

---
+++ DataAdderNew.py
@@ -0,0 +1,177 @@
+
+import csv
+import datetime
+from subprocess import call
+
+#This code takes in the necessary arguments
+
+right_now = datetime.datetime.now().isoformat()          
+list = []
+
+for i in right_now:
+    if i.isnumeric():
+        list.append(i)
+
+tim = ("".join(list))
+
+strlst = []
+namlst = []
+
+print("")
+
+print("Welcome to Table Populator")
+
+print("")
+
+tbl = input("Please enter the name of the table to populate: ")
+
+print("")
+
+val = input("Please enter the name of the .csv file, without the .csv suffix. Please eliminate any headers: ")
+
+print("")
+
+colnum = input("How many columns are there to populate? Note that, if the primary key column is also referenced, that counts as a column, so please add 1 to your total: ")
+
+print("")
+
+valcol = input("Please enter the name of the determinant column holding values: ")
+
+print("")
+
+namlst.append(valcol)
+
+valin = val + ".csv"
+
+content = []
+
+try:
+    with open(valin, 'r') as csvFile:
+        csv_reader = csv.reader(csvFile, delimiter=',')
+        for row in csv_reader:
+            content.append(row[0])
+
+colstr = 0
+
+for elem in content:
+    alph = 0
+        for x in elem:
+            if x.isalpha():
+                 alph = 1
+
+if alph == 1:
+    colstr += 0
+
+if colstr > 0:
+    strlst.append(1)
+
+if colstr == 0:
+    strlst.append(0)
+
+primdat = int(primcoldata)
+
+#This code creates the SQL data to drop values from a .csv into the table
+
+try:
+    infile = open(valin, "r")
+
+except:
+    print("No source csv file by that name can be found.")
+    call(["python", "DataAdder.py"])
+
+infile.close()
+
+valot = tbl + tim + ".csv"
+
+colnm = (int(colnum)-1)
+for x in range(colnm):
+
+    valcolx = input("Please enter the name of a column holding values: ")
+
+    print("")
+
+    namlst.append(valcolx)
+
+    content = []
+
+    try:
+        with open(valin, 'r') as csvFile:
+            csv_reader = csv.reader(csvFile, delimiter=',')
+            for row in csv_reader:
+                content.append(row[x])
+
+    colstr = 0
+
+    for elem in content:
+        alph = 0
+        for x in elem:
+            if x.isalpha():
+                alph = 1
+
+    if alph == 1:
+        colstr += 0
+
+    if colstr > 0:
+        strlst.append(1)
+
+    if colstr == 0:
+        strlst.append(0)
+
+with open(valin, newline='\n') as f_input, open(valot, 'w', newline='\n') as f_output:
+
+    csv_input = csv.reader(f_input)
+    csv_output = csv.writer(f_output)
+
+    for row in csv_input:
+
+        outlst = []
+
+        for x in range(1, int(colnum)):
+
+            xstr = "UPDATE " + tbl + " SET "
+            
+            outlst.append(xstr)
+            
+            ystr = namlst[x]
+            
+            outlst.append(ystr)
+            
+            zstr = " = "
+
+            outlst.append(zstr)
+
+            ostr = row[x]
+            nstr = row[0]
+            nnam = namlst[0]
+
+            if strlst[x] == 1:
+                bstr= "'" + ostr + "'"
+
+            if strlst[x] != 1:
+                bstr = ostr
+
+            outlst.append(bstr)
+
+            xxstr = " WHERE "
+
+            outlst.append(xxstr)
+
+            yystr = nnam + " = " 
+
+            outlst.append(yystr)
+
+            if primcoldata != "1":
+                zzstr = nstr
+
+            if primcoldata == "1":
+                zzstr = "'" + nstr + "'"
+
+            outlst.append(zzstr)
+
+            zzzstr = ";"
+
+            outlst.append(zzzstr)        
+
+        csv_output.writerow(outlst)  
+
+print ("The scripts file should now be available in the same folder as the python code.")
---
+++ RowAdderNew.py
@@ -0,0 +1,116 @@
+import csv
+import datetime
+from subprocess import call
+
+right_now = datetime.datetime.now().isoformat()          
+list = []
+
+for i in right_now:
+    if i.isnumeric():
+        list.append(i)
+
+tim = ("".join(list))
+
+#This code takes in the necessary arguments
+
+print("")
+
+print("Welcome to Table Populator")
+
+print("")
+
+tbl = input("Please enter the name of the table to populate: ")
+
+print("")
+
+val = input("Please enter the name of the .csv file, without the .csv suffix. Please eliminate any headers: ")
+
+print("")
+
+valin = val  + ".csv"
+
+try:
+    with open(valin, 'r') as csvFile:
+        csv_reader = csv.reader(csvFile, delimiter=',')
+        for row in csv_reader:
+            colnum=len(row)
+
+except:
+    print("")
+    print("No file by that name can be found.")
+    call(["python", "RowAdder.py"])
+
+
+strlst = []
+
+for x in range(colnum):
+
+    content = []
+
+    try:
+        with open(valin, 'r') as csvFile:
+            csv_reader = csv.reader(csvFile, delimiter=',')
+            for row in csv_reader:
+                content.append(row[x])
+
+    except:
+    print("")
+    print("No file by that name can be found.")
+    call(["python", "RowAdder.py"])
+
+    colstr = 0
+
+    for elem in content:
+        alph = 0
+            for x in elem:
+                if x.isalpha():
+                    alph = 1
+
+    if alph == 1:
+        colstr += 0
+
+    if colstr > 0:
+        strlst.append(1)
+    if colstr == 0:
+        strlst.append(0)
+
+outfil = tbl + tim + ".csv"
+
+with open(valin, newline='\n') as f_input, open(outfil, 'w', newline='\n') as f_output:
+
+    csv_input = csv.reader(f_input)
+    csv_output = csv.writer(f_output)
+
+    
+
+    for row in csv_input:
+
+        outlst = []
+
+        astr = 'INSERT INTO ' + tbl + " VALUES ("
+
+        outlst.append(astr)
+
+        for x in range(colnum):
+            astr = row[x]
+
+            if strlst[x] == 1:
+                bstr= "'" + astr + "'"
+
+            if strlst[x] != 1:
+                bstr = astr
+
+            if x < (colnum-1):
+                outlst.append(bstr)
+                outlst.append(",")
+
+            if x == (colnum-1):
+                outlst.append(bstr)
+                outlst.append(");") 
+
+        csv_output.writerow(outlst)  
+
+print("Your file can be found in this directory.")
+
+    
+
